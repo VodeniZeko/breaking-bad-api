b9aefb6a00c7996f7abe41db9ff918a9b9297da5
VodeniZeko <salexy.es@gmail.com>
2020-01-30 16:20:03 -0800

    practice

---
+++ bb/src/components/Container.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import Search from "./Search";
+import Card from "./Card";
+import axios from "axios";
+
+const Container = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.breakingbadapi.com/api/characters")
+      .then(res => {
+        setCharacters(res.data);
+      })
+      .catch(err => {
+        console.log("axios error", err);
+      });
+  }, []);
+  console.log(characters);
+  return (
+    <div>
+      <Search />
+      <Card />
+    </div>
+  );
+};
+
+export default Container;
